e059e3e023c7e675dcca8f31b5cd4eab65936383
JoaoGabrielOT <joao_gabrielot@hotmail.com>
2025-09-24 16:44:41 -0300

    Aula_03

--- Aula_03/Senior.py
+++ Aula_03/Senior.py
@@ -5,7 +5,8 @@ for aluno in alunos_e_notas:
     print(aluno)
 
 # ======================================================================
-import random
+from time import sleep
+import os
 
 clientes = []
 loop = True
@@ -13,12 +14,18 @@ def mostrarClientes():
     if clientes:
         print("\nLista atual:")
         for i, cliente in enumerate(clientes):
-            print(f"{i+1}: {cliente}")
+            print(f"""
+{i+1}: Nome: {cliente["nome"]}
+   Numero: {cliente["numero"]}
+   Email: {cliente["email"]}
+            """)
     else:
         print("\nNão ha cliente para imprimir!")
 
+os.system("clear")
 while(loop):
-    print("\n=========================================== Lista de clientes ===========================================")
+    iguais = ("="*43)
+    print(f"\n{iguais} Lista de clientes {iguais}")
     op = str(input("""
     O que você deseja fazer? Digite apenas o número da opção desejada.
 
@@ -29,38 +36,52 @@ while(loop):
     5 - Sair
     
     """))
+    sleep(0.5)
     match op:
         case "1":
-            nome = input("\nDigite o nome do cliente que deseja cadastrar: ")
-            clientes.append(nome)
+            os.system("clear")
+            cliente = {}
+            cliente["nome"] = input("\nDigite o nome do cliente que deseja cadastrar: ")
+            cliente["numero"] = input("\nDigite o numero do cliente: ")
+            cliente["email"] = input("\nDigite o email do cliente: ")
+            clientes.append(cliente)
             mostrarClientes()
+            sleep(0.5)
 
         case "2":
             mostrarClientes()
             delete = input("\nDigite o nome que deseja deletar: ")
             try:
-                clientes.remove(delete)
+                cliente.remove(delete)
                 mostrarClientes()
             except:
                 print("\nNome invalido!!!")
+            sleep(0.5)
                 
                 
         case "3":
+            os.system("clear")
             mostrarClientes()
             atualizar = int(input("\nDigite o numero do nome que deseja atualizar: "))
-            if atualizar-1 >= 0 and atualizar-1 < len(clientes):
-                clientes[atualizar-1] = input("\nDigite o novo nome: ")
+            if atualizar-1 >= 0 and atualizar-1 < len(cliente):
+                cliente[atualizar-1] = input("\nDigite o novo nome: ")
                 mostrarClientes()
             else:
                 print("\nValor invalido!!")
+            sleep(0.5)
                 
                 
         case "4":
+            os.system("clear")
             mostrarClientes()
+            sleep(0.5)
             
 
         case "5":
+            os.system("clear")
             loop = False
             
         case _:
+            os.system("clear")
             print("Opção invalida!!")
+            sleep(0.5)
